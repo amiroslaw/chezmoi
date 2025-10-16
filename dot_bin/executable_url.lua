362c63a744615fb54e1402328bbd012dfcdffbe1
amiroslaw <arek47@gmail.com>
2025-10-16 15:33:22 +0200

    url.lua: add xattrs and embed sub

--- dot_bin/executable_url.lua
+++ dot_bin/executable_url.lua
@@ -186,7 +186,7 @@ end
 
 local function dlAudio(linkTab)
 	os.execute('mkdir -p ' .. AUDIO_DIR)
-	local cmdDlp = 'yt-dlp -q --embed-metadata -f bestaudio -x --audio-format mp3 -o "' .. AUDIO_DIR .. '%%(title)s.%%(ext)s" '
+	local cmdDlp = 'yt-dlp -q --xattrs --embed-metadata -f bestaudio -x --audio-format mp3 -o "' .. AUDIO_DIR .. '%%(title)s.%%(ext)s" '
 	local cmd = 'pueue add --escape --label "%s" -g dl-audio -- ' .. cmdDlp
 	M(linkTab):map(getTitle)
 		:each(M.bindn(execQueue, cmd, 'dl-audio'))
@@ -194,7 +194,7 @@ end
 
 local function dlVideo(linkTab)
 	os.execute('mkdir -p ' .. YT_DIR)
-	local cmdDlp = "yt-dlp -q --embed-metadata -f 'bestvideo[height<=1440]+bestaudio/best' -o '" .. YT_DIR .. "%%(title)s.%%(ext)s' "
+	local cmdDlp = "yt-dlp -q --xattrs --embed-metadata --embed-subs --embed-thumbnail -f 'bestvideo[height<=1440]+bestaudio/best' -o '" .. YT_DIR .. "%%(title)s.%%(ext)s' "
 	local cmd = 'pueue add --escape --label "%s" -g dl-video -- ' .. cmdDlp
 	M(linkTab):map(getTitle)
 		:each(M.bindn(execQueue, cmd, 'dl-video'))
