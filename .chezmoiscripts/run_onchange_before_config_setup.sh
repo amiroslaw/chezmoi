1398406c258266853416579ef7ef337871a0f394
amiroslaw <arek47@gmail.com>
2026-01-05 20:31:39 +0100

    chezmoi fix zsh

--- .chezmoiscripts/run_onchange_before_config_setup.sh
+++ .chezmoiscripts/run_onchange_before_config_setup.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -e
+sudo zypper install -y zsh
+
 chsh -s $(which zsh)
 mkdir -p "$HOME"/Documents "$HOME"/Downloads "$HOME"/Pictures "$HOME"/Videos
+
+# packman
+sudo zypper addrepo -cfp 90 'http://ftp.fau.de/packman/suse/openSUSE_Tumbleweed/' packman
+sudo zypper ref
+sudo zypper dup --from packman --allow-vendor-change
+sudo zypper install -y yad opi
+
 
 # {{ if eq .chezmoi.hostname "pc" }}
 # {{ else if eq .chezmoi.hostname "laptop" }}
