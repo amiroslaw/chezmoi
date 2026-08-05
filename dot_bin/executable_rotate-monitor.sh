314e1e5720e61c8f741b0478af9ececefca8fa20
amiroslaw <arek47@gmail.com>
2026-08-05 16:27:41 +0200

    fix eza and rotate-monitor

--- dot_bin/executable_rotate-monitor.sh
+++ dot_bin/executable_rotate-monitor.sh
@@ -2,11 +2,12 @@
 
 MON="DVI-I-1"
 CURRENT=$(hyprctl monitors -j | fx ".find(m => m.name === \"$MON\").transform")
-
+echo $CURRENT
 if [ "$CURRENT" = "1" ]; then
     # Switch to normal
-    hyprctl keyword monitor "$MON,1920x1080@60.0,1920x0,1.0"
+	hyprctl eval "hl.monitor({ output = '$MON', mode = '1920x1080@60.0', position = '1920x0', scale = 1.0 })"
 else
     # Switch to rotated
-    hyprctl keyword monitor "$MON,1920x1080@60.0,1920x0,1.0,transform,1"
+	hyprctl eval "hl.monitor({ output = 'desc:$MON', mode = '1920x1080@60', position = '1920x0', scale = 1.0, transform = 1 })"
+	hyprctl reload
 fi
